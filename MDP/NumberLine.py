61f0e943f5991e10f9f5c608cf7921c2dc866d78
Hayato Kato <hayatokato8810@gmail.com>
2022-10-25 17:36:59 -0700

    Started implementing PRM

--- MDP/NumberLine.py
+++ MDP/NumberLine.py
@@ -9,13 +9,20 @@ import MDP
 from copy import copy
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 class NumberLine(MDP.MDP):
-	def __init__(self):
-		# State Space
-		self.S = []
+	def __init__(self, start_state, end_state, x_dim, y_dim):
+		self.start_state = start_state
+		self.end_state = end_state
+		# Vertices
+		self.V = [start_state]
+		self.x_dim, self.y_dim = x_dim, y_dim
 
-	def isConnected(self, s1, s2):
+		# Edges
+		self.E = []
+
+	def is_connected(self, s1, s2):
 		y1 = s1[0]
 		v1 = s1[1]
 		y2 = s2[0]
@@ -27,10 +34,19 @@ class NumberLine(MDP.MDP):
 				return a,t
 			else:
 				return None
-
 			pass
 		except:
 			return None
+
+	def prm(self):
+		while self.end_state not in self.V:
+			test_s = (random.random()*self.x_dim, random.random()*self.y_dim)
+			if self.is_connected()
+
+
+
+
+
 
 
 
@@ -39,9 +55,9 @@ def main():
 	print("starting")
 
 	n = NumberLine()
-	print(n.isConnected((0,0),(1,1)))			# The two states are connected via "straight" line
-	print(n.isConnected((0,0),(2,-2))) 		# Has negative time
-	print(n.isConnected((0,0.4),(0.2,0))) # No direct path between the two states
+	print(n.is_connected((0,0),(1,1)))			# The two states are connected via "straight" line
+	print(n.is_connected((0,0),(2,-2))) 		# Has negative time
+	print(n.is_connected((0,0.4),(0.2,0))) # No direct path between the two states
 
 if __name__ == '__main__':
 	main()
