7a4a1d44fa375be126290763da637c1390343a9b
Hayato Kato <hayatokato8810@gmail.com>
2022-10-25 17:57:27 -0700

    Added partial implementation of PRM

--- MDP/NumberLine.py
+++ MDP/NumberLine.py
@@ -42,10 +42,10 @@ class NumberLine(MDP.MDP):
 		distance = np.sqrt((p2[0]-p1[0]**2) + (p2[1]-p1[1]**2))
 		return distance
 	
-	def find_closest(self, V, p1):
+	def find_closest(self, p1):
 		closest_vertex = ()
 		closest_distance = np.Inf
-		for vertex in V:
+		for vertex in self.V:
 			temp_distance = self.find_distance(vertex, p1)
 			if temp_distance < closest_distance:
 				closest_distance = temp_distance
@@ -54,8 +54,16 @@ class NumberLine(MDP.MDP):
 
 	def prm(self):
 		while self.end_state not in self.V:
-			test_s = (random.random()*self.x_dim, random.random()*self.y_dim)
-			if self.is_connected()
+			test_v = None
+			if random.random() > 0.9:
+				test_v = self.end_state
+			else:
+				test_v = (random.random()*self.x_dim, random.random()*self.y_dim)
+			closest_v = self.find_closest(test_v)
+			if self.is_connected(test_v, closest_v):
+				self.V.append(test_v)
+				self.E.append((closest_v,test_v))
+
 
 
 
@@ -63,7 +71,7 @@ class NumberLine(MDP.MDP):
 def main():
 	print("starting")
 
-	n = NumberLine()
+	n = NumberLine((1,1),(11,11),12,12)
 	print(n.is_connected((0,0),(1,1)))			# The two states are connected via "straight" line
 	print(n.is_connected((0,0),(2,-2))) 		# Has negative time
 	print(n.is_connected((0,0.4),(0.2,0))) # No direct path between the two states
