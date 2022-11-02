c7641f7fbef57683f100de1c3bceb743c4c3a281
David <davidkao41@ucla.edu>
2022-11-01 17:51:51 -0700

    update prm

--- MDP/NumberLine.py
+++ MDP/NumberLine.py
@@ -12,31 +12,55 @@ import matplotlib.pyplot as plt
 import random
 
 class NumberLine(MDP.MDP):
-	def __init__(self, start_state, end_state, x_dim, y_dim):
+	def __init__(self, start_state, end_state, x_dim, y_dim, x_delta, y_delta, force):
 		self.start_state = start_state
 		self.end_state = end_state
 		# Vertices
 		self.V = [start_state]
 		self.x_dim, self.y_dim = x_dim, y_dim
-
+		#self.x_min, self.y_min = x_min, y_min
+		#self.x_max, self.y_max = x_max, y_max
+		self.x_delta, self.y_delta = x_delta, y_delta
+		self.force = force #list of possible forces
+		self.mass = 1
+		self.t_max = 20
+		self.amplitude = 1
+		
 		# Edges
 		self.E = []
+	
+	def y_next(self, y, v):
+		return y + v
 
+	def v_next(self, v, input, field):
+		return v + (1/self.mass)*(input+field)
+
+	def field(self, y, amplitude): #assuming field = Acos(y)
+		return amplitude * np.sin(y)
+
+	def is_close(self, s1, s2):
+		if self.find_distance(s1, s2) < 0.1:
+			return True
+		else:
+			return False
+		
 	def is_connected(self, s1, s2):
 		y1 = s1[0]
-		v1 = s1[1]
 		y2 = s2[0]
+		v1 = s1[1]
 		v2 = s2[1]
-		try:
-			a = (v2-v1)*(v2+v1)/(2*(y2-y1)+(v2-v1))
-			t = (v2-v1)/a
-			if t >= 0:
-				return True
-			else:
-				return False
-			pass
-		except:
-			return False
+
+		for f in self.force:
+			t = 0
+			y = y1
+			v = v1
+			for t in range(1, self.t_max):
+				static_field = self.field(y, self.amplitude)
+				y = self.y_next(y,v)
+				v = self.v_next(v, f, static_field)
+				if self.is_close(s2, (y,v)):
+					return True
+		return False
 
 	def find_distance(self, p1, p2): #takes two 2D points
 		distance = np.sqrt((p2[0]-p1[0])*(p2[0]-p1[0]) + (p2[1]-p1[1])*(p2[1]-p1[1]))
@@ -53,7 +77,7 @@ class NumberLine(MDP.MDP):
 		return closest_v
 
 	def prm(self):
-		while self.end_state not in self.V:
+		while self.end_state not in self.V: #
 		#for i in range(1000):
 			test_v = None
 			if random.random() > 0.9:
@@ -88,14 +112,10 @@ class NumberLine(MDP.MDP):
 		plt.show(block=blockingStatus)
 		plt.pause(.1)
 
-
-
-
-
 def main():
 	print("starting")
-
-	n = NumberLine((9,9),(1,1),10,10)
+	force = [x for x in range(-1, 1, 0.1)]
+	n = NumberLine((9,9),(1,1),10,10,0.1,0.1, force)
 	#print(n.is_connected((0,0),(1,1)))			# The two states are connected via "straight" line
 	#print(n.is_connected((0,0),(2,-2))) 		# Has negative time
 	#print(n.is_connected((0,0.4),(0.2,0))) # No direct path between the two states
